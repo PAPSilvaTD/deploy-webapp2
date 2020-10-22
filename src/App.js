409a5c46db8fc3253a12d649ca68d5d7d4d66e5c
PAPSilvaTD <pedro.angelo@talkdesk.com>
2020-10-22 11:56:25 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+		<h1>Movies</h1>
+		<ul>
+			<li>
+				<a href="https://www.imdb.com/find?q=lord+of+the+rings&ref_=nv_sr_sm">LOTR</a>
+				<ul>
+					<li>
+						<a
+							href="https://www.imdb.com/title/tt0120737/?ref_=fn_al_tt_1">Fellowship of the Ring</a>
+					</li>
+					<li>
+            <a href="https://www.imdb.com/title/tt0120737/?ref_=fn_al_tt_1">The Two Towers</a>
+					</li>
+					<li>
+						<a href="https://www.imdb.com/title/tt0120737/?ref_=fn_al_tt_1">Return of the King</a>
+					</li>
+				</ul>
+			</li>
+			<li>
+				Twilight: cannot link to weak plots
+			</li>
+		</ul>
+    </>
   );
 }
 
