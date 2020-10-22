6feb1a4d3a66dc4ba6a05048d33eb1cb8135bc30
PAPSilvaTD <pedro.angelo@talkdesk.com>
2020-10-22 12:15:28 +0100

    Updates

---
+++ precache-manifest.fb31429b25d1fe31da2a6211ec7532c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f087a7b86f9a4684c9a258fc620fb3",
+    "url": "/deploy-webapp2/index.html"
+  },
+  {
+    "revision": "c042e83c1e89d4578e77",
+    "url": "/deploy-webapp2/static/css/main.68a60d15.chunk.css"
+  },
+  {
+    "revision": "bdeb420b02a4795a072b",
+    "url": "/deploy-webapp2/static/js/2.31efd0bd.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/deploy-webapp2/static/js/2.31efd0bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c042e83c1e89d4578e77",
+    "url": "/deploy-webapp2/static/js/main.1e261210.chunk.js"
+  },
+  {
+    "revision": "63f036617d59e6fd67f9",
+    "url": "/deploy-webapp2/static/js/runtime-main.05a54e08.js"
+  }
+]);
